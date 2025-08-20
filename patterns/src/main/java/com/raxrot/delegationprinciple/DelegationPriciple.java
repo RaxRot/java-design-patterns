d463b564e492fffed6946a7962cb7ce49bf5c70a
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-20 19:40:40 +0100

    add DelegationPriciple

---
+++ patterns/src/main/java/com/raxrot/delegationprinciple/DelegationPriciple.java
@@ -0,0 +1,23 @@
+package com.raxrot.delegationprinciple;
+
+public class DelegationPriciple {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.print();
+    }
+}
+
+class RealPrinter{
+    void print(){
+        System.out.println("The delegate");
+    }
+}
+
+class Printer{
+    RealPrinter printer = new RealPrinter();
+    void print(){
+        printer.print();
+    }
+}
+
+
